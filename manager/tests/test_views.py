460ce222649ea7e288b133ddb8560b60b1990a9b
Roman Khudobei <paul.1998.banks@gmail.com>
2021-01-11 23:22:14 +0200

    Write tests for static targets

--- manager/tests/test_views.py
+++ manager/tests/test_views.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
-from manager.models import Source, Target
+from manager.models import Source, Target, StaticTarget
 
 
 class RandomTargetsViewTestCases(TestCase):
@@ -14,6 +14,9 @@ class RandomTargetsViewTestCases(TestCase):
 
     def get_target(self, source, url='test1.com', traffic=0, publish_time=timezone.now()):
         return Target.objects.create(source=source, url=url, traffic=traffic, publish_time=publish_time)
+
+    def get_static_target(self, name='Test', url='static-test.com/', is_active=True):
+        return StaticTarget.objects.create(name=name, url=url, is_active=is_active)
 
     def test_target_returned_in_response(self):
         source = self.get_source()
@@ -85,3 +88,38 @@ class RandomTargetsViewTestCases(TestCase):
                 self.client.get(reverse('manager:random_targets')).content.decode()
             )
             self.assertNotIn('test6.com/', random_targets)
+
+    def test_static_targets_returned_in_response(self):
+        static_target = self.get_static_target()
+
+        random_targets = self.client.get(reverse('manager:random_targets'))
+
+        self.assertJSONEqual(random_targets.content.decode(), [static_target.url])
+
+    def test_not_active_static_targets_is_not_returned(self):
+        self.get_static_target(is_active=False)
+
+        random_targets = self.client.get(reverse('manager:random_targets'))
+
+        self.assertJSONEqual(random_targets.content.decode(), [])
+
+    def test_static_targets_always_returned_in_response(self):
+        source1 = self.get_source(limit=5)
+        self.get_target(source1)
+        self.get_target(source1)
+
+        source2 = self.get_source(name='Test2', url='test2.com/', limit=5)
+        self.get_target(source2)
+        self.get_target(source2)
+
+        static_target1 = self.get_static_target()
+        static_target2 = self.get_static_target(name='Test2', url='test2.com/')
+
+        for _ in range(10):
+            random_targets = json.loads(
+                self.client.get(reverse('manager:random_targets')).content.decode()
+            )
+            self.assertTrue(all([
+                static_target1.url in random_targets,
+                static_target2.url in random_targets,
+            ]))

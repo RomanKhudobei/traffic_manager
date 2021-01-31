32b03edc58de0e643a08c1bc2fc2845437b73c0f
Roman Khudobei <paul.1998.banks@gmail.com>
2021-01-31 20:44:23 +0200

    Fix ordering in statistic view

--- manager/views.py
+++ manager/views.py
@@ -23,7 +23,7 @@ def source_statistic_view(request):
     except (TypeError, ValueError, AssertionError):
         selected_date = dates[0] if dates else dt.date.today()
 
-    targets = source.targets.filter(created_at__date=selected_date).order_by('-created_at')
+    targets = source.targets.filter(created_at__date=selected_date).order_by('-publish_time')
 
     return render(request, template_name='manager/source_statistic.html', context={
         'source_name': source_name,

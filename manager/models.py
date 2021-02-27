8d20d9288c2247f24a2c75239b814ce088a50c6b
Roman Khudobei <roman.khudobei@gmail.com>
2021-02-27 16:25:44 +0200

    Update model docstrings

--- manager/models.py
+++ manager/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class Source(models.Model):
     """
-    Source of targets. Basically url to site feed
+    Source of targets. Basically url to site RSS feed
     """
     name = models.CharField(max_length=50)
     url = models.URLField(unique=True, help_text='Посилання на RSS стрічку')
@@ -17,7 +17,7 @@ class Source(models.Model):
 
 class Target(models.Model):
     """
-    Url, which will receive traffic
+    Url, which will receive traffic. Url, that going to be pasted to iframe
     """
     source = models.ForeignKey(Source, related_name='targets', on_delete=models.CASCADE)
     title = models.CharField(max_length=200, default='Немає')

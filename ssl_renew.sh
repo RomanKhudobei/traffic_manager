095723b68a3a1639d0508877ea97f89e76902489
RomanKhudobei <r.khudobei@mobidev.biz>
2022-09-25 02:19:15 +0300

    Fix ssl renew script

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -4,5 +4,5 @@ COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
 cd ~/traffic_manager
-$COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
+$COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP nginx
 $DOCKER system prune -af
